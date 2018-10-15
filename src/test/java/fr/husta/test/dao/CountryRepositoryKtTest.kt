8ac937621afcec7272beab58604fb4af9d98f523
Guillaume HUSTA <guillaume.husta@gmail.com>
2018-10-15 17:09:33 +0200

    Add a Kotlin JUnit test running with SpringRunner

---
+++ src/test/java/fr/husta/test/dao/CountryRepositoryKtTest.kt
@@ -0,0 +1,31 @@
+package fr.husta.test.dao
+
+import fr.husta.test.config.DatabaseConfiguration
+import fr.husta.test.database.TestDatabase
+import fr.husta.test.domain.Country
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.annotation.Rollback
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringRunner
+import kotlin.test.Test
+
+@RunWith(SpringRunner::class)
+@ContextConfiguration(classes = arrayOf(DatabaseConfiguration::class))
+@ActiveProfiles("TEST")
+@TestDatabase  // includes @Transactional
+@Rollback
+class CountryRepositoryKtTest {
+
+    @Autowired
+    lateinit var countryRepository: CountryRepository
+
+    @Test
+    fun `find by name not null`() {
+        val countries = countryRepository.findByName("France")
+        assertThat<Country>(countries).isNotEmpty()
+    }
+
+}
